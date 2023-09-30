fe9eeaa77efb1d5263e814a71f97cf1526de12e2
JRaamos <jhonyramos46@gmail.com>
2023-09-30 00:01:58 -0300

    feat: adiciona logo, botão para pagina home

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 
-function App(){
+function App() {
 
   return (
     <div>
--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import logo from "../images/logo-1.png";
 
 export default function Home() {
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container-home" >
+      <section className="section-logo">
+        <img src={logo} alt="logo" className="img-logo" />
+      </section>
+      <section>
+      <p className="p-text">Escolha seu Barbeiro</p>
+        <button className="barbeiro"> Barbeiro Cleberson Silva (CHUCA) </button>
+      </section>
+      <footer className="footer">
+        <p>© 2023 Jonathan Febraio. All rights reserved.</p>
+      </footer>
     </div>
-  )
+  );
 }
