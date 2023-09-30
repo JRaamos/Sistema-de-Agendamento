1212aaf5ac12e43eb64227e2aaba07270785b298
JRaamos <jhonyramos46@gmail.com>
2023-09-30 00:47:09 -0300

    feat: adiciona stylo a pagina home

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,9 +5,9 @@ import Home from './pages/Home'
 function App() {
 
   return (
-    <div>
+    <div >
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </div>
   )
--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,16 +1,19 @@
 import logo from "../images/logo-1.png";
+import "../styles/home.css";
 
 export default function Home() {
 
   return (
-    <div className="container-home" >
+    <div className="container-home">
+      <div className="container-logo-button">
       <section className="section-logo">
         <img src={logo} alt="logo" className="img-logo" />
       </section>
       <section>
-      <p className="p-text">Escolha seu Barbeiro</p>
+        <p className="p-text">Escolha seu Barbeiro</p>
         <button className="barbeiro"> Barbeiro Cleberson Silva (CHUCA) </button>
       </section>
+      </div>
       <footer className="footer">
         <p>© 2023 Jonathan Febraio. All rights reserved.</p>
       </footer>
