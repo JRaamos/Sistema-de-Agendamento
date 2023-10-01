53a7b45d4015644434252c10d6193d56d0ed4c91
JRaamos <jhonyramos46@gmail.com>
2023-10-01 00:38:35 -0300

    fix: resolve problema de perda de codigo

--- front-end/src/pages/Agendamentos.tsx
+++ front-end/src/pages/Agendamentos.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import messagensInicials from "../utils/mensagens";
+import "../styles/agendamentos.css";
+
 function Agendamentos() {
   const [name, setName] = useState("");
   const [isName, setIsName] = useState(false);
   const [disableButton, setDisableButton] = useState(true);
+  const [text, setText] = useState("");
+  const [text2, setText2] = useState("");
+  const [istext, setIsText] = useState(false);
 
+  const textName = `Seja bem vindo ${name} a Stylus Barbearia`;
   const nameAndButton = (name: string) => {
     setName(name);
     if (name.length > 3) {
@@ -13,33 +19,64 @@ function Agendamentos() {
       setDisableButton(true);
     }
   };
+  useEffect(() => {
+    const textoArray1 = messagensInicials.mensagem01.split("");
+    const textoArray2 = messagensInicials.mensagem02.split("");
+    let currentText1 = "";
+    let currentText2 = "";
+
+    const typingInterval = setInterval(() => {
+      if (textoArray1.length > 0) {
+        currentText1 += textoArray1.shift();
+        setText(currentText1);
+      } else if (textoArray2.length > 0) {
+        setIsText(true);
+        currentText2 += textoArray2.shift();
+        setText2(currentText2);
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 30);
+
+    return () => clearInterval(typingInterval);
+  }, []);
+
   return (
-    <div>
-      <section className="section-mensagem">
-        <p>{messagensInicials.mensagem01}</p>
-      </section>
-      <section className="section-mensagem">
-        <p>{messagensInicials.mensagem02}</p>
-      </section>
+    <div className="container-agendamentos">
+      <div>
+        <section className="section-mensagem">
+          <div>
+            <p className="digitando">{text}</p>
+          </div>
+        </section>
+        {istext && (
+          <section className="section-mensagem">
+            <p>{text2}</p>
+          </section>
+        )}
+      </div>
       {isName && (
         <section className="section-mensagem-usuario">
-          <p>{name}</p>
+          <section className="section-name">
+            <p>{name}</p>
+          </section>
         </section>
       )}
       {isName && (
         <div>
           <section className="section-mensagem">
-            <p>{`Seja bem vindo ${name} a Stylus Barbearia`}</p>
+            <p>{messagensInicials.mensagem03(name)}</p>
           </section>
+
           <section className="section-mensagem">
             <p>{messagensInicials.mensagem04}</p>
           </section>
         </div>
       )}
-      <form>
+      <form className="rodape">
         <label htmlFor="input-usuario">
           <input
-            id="input-usuario"
+            className="input-usuario"
             onChange={({ target }) => {
               nameAndButton(target.value);
             }}
@@ -48,6 +85,7 @@ function Agendamentos() {
         </label>
         <button
           type="button"
+          className="button-usuario"
           onClick={() => setIsName(true)}
           disabled={disableButton}
         >
--- front-end/src/utils/mensagens.ts
+++ front-end/src/utils/mensagens.ts
@@ -3,6 +3,7 @@ const mensagensInicial = {
   mensagem01: `Olá, tudo bem? Sou a assistente virtual
    do barbeiro CHUCA e cuido do agendamento dos serviços dele, blz?`,
   mensagem02: 'Por favor, me informe seu nome e sobrenome.',
+  mensagem03: (nome: string) => `Seja bem vindo ${nome} a Stylus Barbearia`,
   mensagem04: 'Qual serviço você deseja realizar?',
 };
 export default mensagensInicial;
