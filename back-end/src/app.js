5f331e4f3284de2e0dc21f55cfdf4dd189f4b414
JRaamos <jhonyramos46@gmail.com>
2023-10-01 00:28:33 -0300

    feat: adiciona configuração de cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const cors = require('cors');
 const router = require('./routers');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router.barberServicesRouter);
 
