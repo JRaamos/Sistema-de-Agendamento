bb4e5401fe4af25e82ec1324e48241d2ed85b912
JRaamos <jhonyramos46@gmail.com>
2023-09-30 23:52:04 -0300

    fix: resolve problema de rederização da rota /services e adiciona automação de script

--- back-end/src/controllers/barberServices.controller.js
+++ back-end/src/controllers/barberServices.controller.js
@@ -2,11 +2,9 @@ const { barberServices } = require('../services');
 
 const getAllBarberServicesController = async (_req, res) => {
   const services = await barberServices.getAllBarberServices();
-  console.log(services);
   return res.status(200).json(services);
 };
 
-getAllBarberServicesController();
 
 module.exports = {
   getAllBarberServicesController,
--- back-end/src/routers/barberServices.router.js
+++ back-end/src/routers/barberServices.router.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 
 const { barberServicesController } = require('../controllers');
 
-router.get('/', () => "Seja bem vindo ao nosso sistema de agendamento de serviços da Barberia Stylu's!");
 
-router.get('/services', () => barberServicesController.getAllBarberServices);
+router.get('/services', barberServicesController.getAllBarberServicesController);
 
 module.exports = router;
