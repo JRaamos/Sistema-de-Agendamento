cee052fcc56f7f65b55d1f7e730a0537584b4fab
JRaamos <jhonyramos46@gmail.com>
2023-10-21 15:50:03 -0300

    feat: adiciona fetch

--- front-end/src/utils/fetchApi.ts
+++ front-end/src/utils/fetchApi.ts
@@ -1,6 +1,16 @@
-const fetchAPi = async () => {
-  const response = await fetch('https://psychotic-direction-production.up.railway.app/services');
-  const data = await response.json();  
+import { Values } from "../types/AgendamentosProvider";
+
+const fetchAPi = async (values: Values) => {
+  const { agendamentos, ...newValues } = values;
+  const response = (await fetch('http://localhost:3001/registre', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newValues)
+  }
+  ));
+
+  const data = await response.json();
+  
   return data;
 }
 export default fetchAPi;
