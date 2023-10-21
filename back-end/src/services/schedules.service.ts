37210c33c2fd156a4b607694baeb2a5a66638e53
JRaamos <jhonyramos46@gmail.com>
2023-10-21 14:01:50 -0300

    fix: muda retorno da função de criação de schedules

--- back-end/src/services/schedules.service.ts
+++ back-end/src/services/schedules.service.ts
@@ -1,20 +1,12 @@
 import ScheduleModel, { ScheduleInputtableTypes } from '../database/models/schedules.model';
-import { ServiceResponse } from '../types/ServiceResponse';
 import { Schedule } from '../types/schedules';
 
 const createSchedule = async (schedule: ScheduleInputtableTypes):
-Promise<ServiceResponse<Schedule>> => {
+Promise<Schedule> => {
   const { date, hour, userId, serviceId } = schedule;
 
-  if (!date || !hour || !userId || !serviceId) {
-    return {
-      status: 'INVALID_DATA',
-      data: { message: 'Date, hour, userId and serviceId are required' },
-    };
-  }
-
   const scheduleResult = await ScheduleModel.create({ date, hour, userId, serviceId });
-  return { status: 'SUCCESSFUL', data: scheduleResult.dataValues };
+  return scheduleResult.dataValues as Schedule;
 };
 
 export default { createSchedule };
