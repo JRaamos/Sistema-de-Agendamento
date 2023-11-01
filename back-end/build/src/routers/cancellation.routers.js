5a192936ad8b480401646aed9c03eeec4ce48e7e
JRaamos <jhonyramos46@gmail.com>
2023-11-01 00:36:00 -0300

    testando

--- back-end/build/src/routers/cancellation.routers.js
+++ back-end/build/src/routers/cancellation.routers.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const cancellation_controller_1 = __importDefault(require("../controllers/cancellation.controller"));
+const validateJWT_1 = __importDefault(require("../middlewares/validateJWT"));
 const routerCancellation = (0, express_1.Router)();
 routerCancellation.post('/cancellation', cancellation_controller_1.default.createCancellation);
-routerCancellation.get('/cancellation/:intervaloDias', cancellation_controller_1.default.countCancellation);
+routerCancellation.get('/cancellation/:intervalDays', validateJWT_1.default, cancellation_controller_1.default.countCancellation);
 exports.default = routerCancellation;
