5027058fca1c986893cd92c3c7606a98cf6826f7
JRaamos <jhonyramos46@gmail.com>
2023-11-02 12:11:56 -0300

    feat: adiciona CSS para pagina de login

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logo from "../images/logo-1.png";
 import { fetchAPiLogin } from "../utils/fetchApi";
 import { useNavigate } from "react-router-dom";
+import "../styles/login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -16,56 +17,70 @@ function Login() {
       setMensage(token.mensage);
       return;
     }
-
     localStorage.setItem("token", token.mensage);
     navigate("/barbers");
   };
 
   return (
-    <div>
+    <div className="login-container">
       <section className="section-logo">
-        <img src={logo} alt="logo" className="img-logo" />
+        <img src={logo} alt="logo" className="img-logo img-logo-login" />
       </section>
-      {mensage.length > 0 && <p>{mensage}</p>}
-      <form>
-        <label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            required
-            onChange={({ target }) => {
-              setEmail(target.value);
-              setMensage("");
-            }}
-            placeholder="Digite seu Email"
-          />
-        </label>
-        <label>
-          <input
-            type={showPassword ? "text" : "password"}
-            name="password"
-            value={password}
-            required
-            onChange={({ target }) => {
-              setPassword(target.value);
-              setMensage("");
-            }}
-            placeholder="Digite sua Senha"
-          />
-          <button
-            type="button"
-            aria-label={showPassword ? "Ocultar" : "Mostrar"}
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? "Ocultar" : "Mostrar"}
+      <form className="form-login">
+        <h1 className="title-login">Login</h1>
+        <div className="message-container">
+          {mensage.length > 0 && (
+            <p
+              className={`mensagem-erro ${mensage.length > 0 ? "active" : ""}`}
+            >
+              {mensage}
+            </p>
+          )}
+        </div>
+        <div>
+          <div className="input-group">
+            <input
+              className="input-text"
+              type="email"
+              name="email"
+              value={email}
+              required
+              onChange={({ target }) => {
+                setEmail(target.value);
+                setMensage("");
+              }}
+              placeholder="Digite seu Email"
+            />
+          </div>
+          <div className="input-group">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="input-text"
+              name="password"
+              value={password}
+              required
+              onChange={({ target }) => {
+                setPassword(target.value);
+                setMensage("");
+              }}
+              placeholder="Digite sua Senha"
+            />
+            <button
+              type="button"
+              className="button-toggle-password"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? "Ocultar" : "Mostrar"}
+            </button>
+          </div>
+          <button type="button" className="button-login" onClick={verifyLogin}>
+            Entrar
           </button>
-        </label>
-
-        <button type="button" onClick={verifyLogin}>
-          Entrar
-        </button>
+        </div>
       </form>
+      <footer className="footer">
+        <p>© 2023 Jonathan Febraio. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
