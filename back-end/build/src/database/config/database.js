bf8e96ab8dba5db5718a6f4b1fcaed666520db9f
JRaamos <jhonyramos46@gmail.com>
2023-11-06 17:32:30 -0300

    fix: reajusta Dokerfile back-end

--- back-end/build/src/database/config/database.js
+++ back-end/build/src/database/config/database.js
@@ -1,10 +1,10 @@
 "use strict";
 const config = {
-    username: process.env.MYSQL_ADDON_USER,
-    password: process.env.MYSQL_ADDON_PASSWORD,
-    database: process.env.MYSQL_ADDON_DB,
-    host: process.env.MYSQL_ADDON_HOST,
-    port: Number(process.env.MYSQL_ADDON_PORT),
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: Number(process.env.MYSQLPORT),
     dialect: 'mysql',
 };
 module.exports = config;
