86275e294606c42f7bb6ec98d272a40acaadb2d6
JRaamos <jhonyramos46@gmail.com>
2023-11-13 16:39:52 -0300

    fix: ajusta configuração para uso do banco de dados em produção

--- back-end/src/database/config/database.ts
+++ back-end/src/database/config/database.ts
@@ -1,24 +1,24 @@
-// import { Options } from 'sequelize';
-
-// const config: Options = {
-//   username: process.env.MYSQL_ADDON_USER,
-//   password: process.env.MYSQL_ADDON_PASSWORD,
-//   database: process.env.MYSQL_ADDON_DB,
-//   host: process.env.MYSQL_ADDON_HOST,
-//   port: Number(process.env.MYSQL_ADDON_PORT),
-//   dialect: 'mysql',
-// };
-
-// export = config;
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  host: process.env.MYSQLHOST,
-  port: Number(process.env.MYSQLPORT),
+  username: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  host: process.env.MYSQL_ADDON_HOST,
+  port: Number(process.env.MYSQL_ADDON_PORT),
   dialect: 'mysql',
 };
 
 export = config;
+// import { Options } from 'sequelize';
+
+// const config: Options = {
+//   username: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   host: process.env.MYSQLHOST,
+//   port: Number(process.env.MYSQLPORT),
+//   dialect: 'mysql',
+// };
+
+// export = config;
